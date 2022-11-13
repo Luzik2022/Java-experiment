e6c8a4f5323da692416e0a62da1314aac4e92f7c
irina <luzik.i@mail.ru>
2022-11-13 16:25:48 +0300

    Homework for lesson3

---
+++ src/main/java/lesson3/homework3.java
@@ -0,0 +1,75 @@
+package lesson3;
+
+import java.util.Arrays;
+
+public class homework3 {
+
+    public static void main(String[] args) {
+        int[] arr1 = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
+        System.out.println(Arrays.toString(arr1));
+        invertArray(arr1);
+        System.out.println(Arrays.toString(arr1));
+        fillArray(100);
+        System.out.println();
+        int[] arr2 = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+        changeArray(arr2);
+        fillDiagonals();
+        create(6, 2);
+    }
+
+
+    // Заменить 0 на 1, 1 на 0;
+    private static void invertArray(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 0) {
+                arr[i] = 1;
+            } else {
+                arr[i] = 0;
+            }
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    private static void fillArray(int size) {
+        int[] arr = new int[size];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = i + 1;
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    private static void changeArray(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < 6) {
+                arr[i] = arr[i] * 2;
+            }
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    private static void fillDiagonals() {
+        int[][] arr = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}};
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (i == j) {
+                    arr[i][j] = 1;
+                }
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    private static void create(int len, int initialValue) {
+        int[] arr = new int[len];
+        for (int i = 0; i < len; i++) {
+            arr[i] = initialValue;
+            System.out.print("[" + i + "]" + arr[i] + " ");
+        }
+    }
+
+}
+
